6ce06731f1e7ffcfc9f27e7644af95d33f61886a
fokses <fokses@gmail.com>
2023-01-19 09:41:58 +0200

    HW0_Opt2_WaitNotify

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -9,6 +9,7 @@ import java.time.LocalTime;
 import java.time.Month;
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class UserMealsUtil {
@@ -19,7 +20,6 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
-                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 11, 59, 59), "Второй завтрак", 500),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
         );
@@ -31,45 +31,39 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with excess. Implement by cycles
+        final List<UserMealWithExcess> result = new ArrayList<>();
+        final Map<LocalDate, Integer> excessTable = new HashMap<>();
+        final AtomicInteger counter = new AtomicInteger();
 
-        List<UserMealWithExcess> result = Collections.synchronizedList(new ArrayList<>());
-        Map<LocalDate, Integer> excessTable = new HashMap<>();
-        final CountDownLatch LATCH = new CountDownLatch(meals.size());
-        ExecutorService service = Executors.newCachedThreadPool();
-
-        for(UserMeal meal : meals) {
-            excessTable.merge(meal.getDateTime().toLocalDate(), meal.getCalories(), Integer::sum);
-
-            if (TimeUtil.isBetweenHalfOpen(meal.getDateTime().toLocalTime(), startTime, endTime)) {
-                service.submit(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            LATCH.await();
+        synchronized (result) {
+            for (UserMeal meal : meals) {
+                excessTable.merge(meal.getDateTime().toLocalDate(), meal.getCalories(), Integer::sum);
+                if (TimeUtil.isBetweenHalfOpen(meal.getDateTime().toLocalTime(), startTime, endTime)) {
+                    new Thread(() -> {
+                        synchronized (result) {
                             result.add(new UserMealWithExcess(meal, excessTable.getOrDefault(meal.getDateTime().toLocalDate(), 0) > caloriesPerDay));
-                        } catch (InterruptedException ignore) {}
-                    }
-                });
+                            counter.incrementAndGet();
+                            result.notify();
+                        }
+                    }).start();
+                } else {
+                    counter.incrementAndGet();
+                }
             }
-
-            LATCH.countDown();
         }
 
-        service.shutdown();
-        while(!service.isShutdown()) {
-
-            try {
-                Thread.currentThread().wait(1);
-            } catch (InterruptedException ignore) {}
+        synchronized (result) {
+            while (counter.get() < meals.size()) {
+                try {
+                    result.wait();
+                } catch (InterruptedException ignore) {}
+            }
         }
 
         return result;
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO Implement by streams
-
         Map<LocalDate, Integer> excessTable = new HashMap<>();
         meals.stream()
                 .forEach(x -> excessTable.merge(x.getDateTime().toLocalDate(), x.getCalories(), Integer::sum));
