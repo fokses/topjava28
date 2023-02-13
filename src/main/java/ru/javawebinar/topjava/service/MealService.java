7c9fde5114b49111222eb18cbf13f99029be7aab
fokses <fokses@gmail.com>
2023-02-13 16:57:05 +0200

    hw2

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,44 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
+
+@Service
 public class MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
 
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    public Meal create(int userId, Meal m) {
+        return repository.save(userId, m);
+    }
+
+    public void delete(int userId, int mealId) {
+        checkNotFoundWithId(repository.delete(userId, mealId), mealId);
+    }
+
+    public Meal get(int userId, int mealId) {
+        return checkNotFoundWithId(repository.get(userId, mealId), mealId);
+    }
+
+    public List<Meal> getAll(int userId) {
+        return repository.getAll(userId);
+    }
+
+    public List<Meal> getAll(int userId, LocalDate start, LocalDate end) {
+        return repository.getAll(userId, start, end);
+    }
+
+    public void update(int userId, Meal m) {
+        checkNotFoundWithId(repository.save(userId, m), m.getId());
+    }
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,63 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+
+@Controller
 public class MealRestController {
-    private MealService service;
+    private  final Logger log = LoggerFactory.getLogger(getClass());
+    private final MealService service;
 
+    public MealRestController(MealService service) {
+        this.service = service;
+    }
+
+    public List<MealTo> getAll() {
+        log.info("getAll: {}", SecurityUtil.authUserId());
+        return MealsUtil.getTos(service.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserCaloriesPerDay());
+    }
+
+    public List<MealTo> getAll(LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime) {
+        log.info("getAll: {}; filters {} {} {} {}", SecurityUtil.authUserId(), startDate, endDate, startTime, endTime);
+
+
+        return MealsUtil.getFilteredTos(
+                service.getAll(SecurityUtil.authUserId(),
+                        (startDate == null ? LocalDate.MIN : startDate), (endDate == null ? LocalDate.MAX : endDate)),
+                SecurityUtil.authUserCaloriesPerDay(),
+                (startTime == null ? LocalTime.MIN : startTime),
+                (endTime == null ? LocalTime.MAX : endTime)
+        );
+
+    }
+
+    public void delete(int mealId) {
+        log.info("delete: {}", mealId);
+        service.delete(SecurityUtil.authUserId(), mealId);
+    }
+
+    public Meal create(Meal m) {
+        log.info("create: {}", m);
+        checkNew(m);
+        return service.create(SecurityUtil.authUserId(), m);
+    }
+
+    public void update(Meal m) {
+        log.info("update meal {} with id {}", m, m.getId());
+        assureIdConsistent(m, m.getId());
+        service.update(SecurityUtil.authUserId(), m);
+    }
 }
