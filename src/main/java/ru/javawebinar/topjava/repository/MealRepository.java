46fa0ca50282dec70104ddce1863848738901438
fokses <fokses@gmail.com>
2023-02-13 16:57:05 +0200

    hw2

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -2,19 +2,22 @@ package ru.javawebinar.topjava.repository;
 
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDate;
 import java.util.List;
 
 // TODO add userId
 public interface MealRepository {
     // null if updated meal does not belong to userId
-    Meal save(Meal meal, int userId);
+    Meal save(int userId, Meal meal);
 
     // false if meal does not belong to userId
-    boolean delete(int id, int userId);
+    boolean delete(int userId, int id);
 
     // null if meal does not belong to userId
-    Meal get(int id, int userId);
+    Meal get(int userId, int id);
 
     // ORDERED dateTime desc
     List<Meal> getAll(int userId);
+
+    List<Meal> getAll(int userId, LocalDate start, LocalDate end);
 }
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,26 +1,30 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.DateTimeUtil;
 import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.web.SecurityUtil;
 
+import java.time.LocalDate;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
+@Repository
 public class InMemoryMealRepository implements MealRepository {
     private final Map<Integer, Map<Integer, Meal>> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
         for (Meal m : MealsUtil.meals)
-            save(m, SecurityUtil.authUserId());
+            save(SecurityUtil.authUserId(), m);
     }
 
     @Override
-    public Meal save(Meal meal, int userId) {
+    public Meal save(int userId, Meal meal) {
         Map<Integer, Meal> userDb = getUserDb(userId);
         Objects.requireNonNull(userDb);
         if (meal.isNew()) {
@@ -33,7 +37,7 @@ public class InMemoryMealRepository implements MealRepository {
     }
 
     @Override
-    public boolean delete(int id, int userId) {
+    public boolean delete(int userId, int id) {
 
         Map<Integer, Meal> userDb = getUserDb(userId);
         Objects.requireNonNull(userDb);
@@ -41,19 +45,25 @@ public class InMemoryMealRepository implements MealRepository {
     }
 
     @Override
-    public Meal get(int id, int userId) {
+    public Meal get(int userId, int id) {
         Map<Integer, Meal> userDb = getUserDb(userId);
         Objects.requireNonNull(userDb);
         return userDb.get(id);
     }
 
     @Override
-    public List<Meal> getAll(int userId) {
+    public List<Meal> getAll(int userId, LocalDate start, LocalDate end) {
         Map<Integer, Meal> userDb = getUserDb(userId);
         Objects.requireNonNull(userDb);
         return userDb.values().stream()
+                .filter(m -> DateTimeUtil.isBetween(m.getDate(), start, end))
                 .sorted(Comparator.comparing(Meal::getDateTime).reversed())
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Meal> getAll(int userId) {
+        return getAll(userId, LocalDate.MIN, LocalDate.MAX);
     }
 
     private Map<Integer, Meal> getUserDb(int userId) {
