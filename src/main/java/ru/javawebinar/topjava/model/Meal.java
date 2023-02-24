511dda24e3010d583584186298e87417325d2639
fokses <fokses@gmail.com>
2023-02-24 21:45:05 +0200

    added named query

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,38 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@Entity
+@Table(name = "Meal", uniqueConstraints = {
+        @UniqueConstraint(name = "uc_meal_date_time_user_id", columnNames = {"date_time", "user_id"})
+})
+@NamedQueries({
+        @NamedQuery(name = "Meal.GET", query = "SELECT m FROM Meal m WHERE m.id = :id AND m.user.id = :userId"),
+        @NamedQuery(name = "Meal.GET_BETWEEN", query = "SELECT m FROM Meal m WHERE m.user.id = :userId AND m.dateTime >= :startDateTime " +
+                "AND m.dateTime < :endDateTime ORDER BY m.dateTime DESC"),
+        @NamedQuery(name = "Meal.GET_ALL", query = "SELECT m FROM Meal m WHERE m.user.id = :userId ORDER BY m.dateTime DESC"),
+        @NamedQuery(name = "Meal.DELETE", query = "DELETE FROM Meal m WHERE m.id = :id AND m.user.id = :userId")
+})
 public class Meal extends AbstractBaseEntity {
+    @NotNull
+    @Column(name = "date_time", nullable = false)
     private LocalDateTime dateTime;
 
+    @Column(nullable = false)
+    @NotBlank
     private String description;
 
+    @Column(nullable = false)
     private int calories;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(nullable = false)
     private User user;
 
     public Meal() {
