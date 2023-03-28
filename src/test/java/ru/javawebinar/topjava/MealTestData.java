e628ae258a7c5ff62ab2dbc97f2440ed2bbb6b7e
fokses <fokses@gmail.com>
2023-03-28 16:30:19 +0300

    MealRestControllerTest

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.to.MealTo;
 
+import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
@@ -29,6 +30,10 @@ public class MealTestData {
     public static final Meal adminMeal2 = new Meal(ADMIN_MEAL_ID + 1, of(2020, Month.JANUARY, 31, 21, 0), "Админ ужин", 1500);
 
     public static final List<Meal> meals = List.of(meal7, meal6, meal5, meal4, meal3, meal2, meal1);
+
+    public static final LocalDateTime startDateTime = LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0, 0);
+
+    public static final LocalDateTime endDateTime = LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0, 0);
 
     public static Meal getNew() {
         return new Meal(null, of(2020, Month.FEBRUARY, 1, 18, 0), "Созданный ужин", 300);
